44f9c085b18fb854752948cf22e4d7e1546e60d5
Nikolaev Tomov <nikolaevtomov@Nikolaevs-MacBook-Pro.local>
2015-12-17 17:14:16 +0000

    Updating layout

--- src/app/components/carousel/carousel.directive.js
+++ src/app/components/carousel/carousel.directive.js
@@ -8,37 +8,40 @@ angular
   .directive('carouselar', carouselar);
 
   function carouselar($window, CarouselarConstants) {
-        return {
-          replace: true,
-          restrict: 'AE',
-          scope: {
-            images: '='
-          },
-          templateUrl: 'app/components/carousel/carousel.html',
-          controller: 'CarouselarController',
-          controllerAs: 'vm',
-          link: function(scope, element, attrs) {
 
-            scope.vm.maxImageCount = Math.min(
-            parseInt(attrs.displayingImageCount), CarouselarConstants.MAX_DISPLAYING_IMAGE) || 1;
+    return {
+      replace: true,
+      restrict: 'AE',
+      scope: {
+        images: '='
+      },
+      templateUrl: 'app/components/carousel/carousel.html',
+      controller: 'CarouselarController',
+      controllerAs: 'vm',
+      link: function(scope, element, attrs) {
 
-            var win = angular.element($window);
-            win.bind('resize', scope.onResize);
-            scope.onResize();
+        scope.vm.maxImageCount = Math.min(
+        parseInt(attrs.displayingImageCount), CarouselarConstants.MAX_DISPLAYING_IMAGE) || 1;
 
-            scope.$on('$destroy', function() {
-              win.unbind('resize', scope.onResize);
-            });
+        var win = angular.element($window);
+        win.bind('resize', scope.onResize);
+        scope.onResize();
 
-          }
-        };
+        scope.$on('$destroy', function() {
+          win.unbind('resize', scope.onResize);
+        });
+
       }
+    };
+
+  }
 
 angular
   .module('moto')
   .directive('carouselarImage', carouselarImage);
 
   function carouselarImage($compile) {
+
     return {
       restrict: 'E',
       scope: {
@@ -49,7 +52,7 @@ angular
         tag: '@',
         headingUrl: '@'
       },
-      template: '<div class="carouselar__loader" ng-if="vm.isLoading"></div>',
+      template: '<div class="carouselar__container__inner__thumbs__loader" ng-if="vm.isLoading"></div>',
       controller: 'CarouselarImageController',
       controllerAs: 'vm',
       link: function(scope, element) {
@@ -57,22 +60,23 @@ angular
         scope.vm.compileImage = function() {
 
           element.append(
-              '<img class="carouselar__image" carouselar-loader on-load="vm.onLoad()" ng-src="{{imgUrl}}"/>');
+              '<img class="carouselar__container__inner__thumbs__image" carouselar-loader on-load="vm.onLoad()" ng-src="{{imgUrl}}"/>');
 
           element.append(
-              '<date class="carouselar__date">{{date}}</date>');
+              '<date class="carouselar__container__inner__thumbs__date">{{date}}</date>');
 
           element.append(
-              '<h4 class="carouselar__heading"><a ng-href="#/news/{{categoryUrl}}/{{headingUrl}}">{{headingUrl}}</a></h4>');
+              '<h4 class="carouselar__container__inner__thumbs__heading"><a ng-href="#/news/{{categoryUrl}}/{{headingUrl}}">{{headingUrl}}</a></h4>');
 
           element.append(
-              '<span class="carouselar__cat"><a ng-href="#/news/{{tag}}">{{tag}}</a></span>');
+              '<span class="carouselar__container__inner__thumbs__cat"><a ng-href="#/news/{{tag}}">{{tag}}</a></span>');
 
           $compile(element.contents())(scope);
 
         };
       }
     };
+
   }
 
 angular
@@ -80,6 +84,7 @@ angular
   .directive('carouselarLoader', carouselarLoader);
 
   function carouselarLoader() {
+
     return {
       restrict: 'A',
       scope: {
@@ -89,6 +94,7 @@ angular
         element.bind('load', scope.onLoad);
       }
     };
+
   }
 
 })();
